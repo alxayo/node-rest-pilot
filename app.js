6917374a6b68e1d887894ffb07c5ea281cc641fa
alxayo <alex.yochev@gmail.com>
2022-11-07 16:45:48 +0000

    Express REST API with Mongo DB

--- app.js
+++ app.js
@@ -1,36 +1,51 @@
-//node.js web server
+// Node Express Server REST API
 
-//require http module
-var http = require('http');
-//require url module
-var url = require('url');
-//require file system module
-var fs = require('fs');
+// Dependencies
+var express = require('express');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
-//create server
-http.createServer(function (req, res) {
-    //parse url
-    var q = url.parse(req.url, true);
-    //get filename
-    var filename = "." + q.pathname;
-    //read file
-    fs.readFile(filename, function(err, data) {
-        //if error
-        if (err) {
-            //set response header
-            res.writeHead(404, {'Content-Type': 'text/html'});
-            //return 404 message
-            return res.end("404 Not Found");
-        }
-        //set response header
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        //write file to response
-        res.write(data);
-        //end response
-        return res.end();
-    });
-//listen on port 8080
-}).listen(8080);
+// Express
+var app = express();
 
-//log to console
-console.log('Server runnin');
+// Body Parser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Mongoose
+mongoose.connect('mongodb://localhost:27017/test');
+
+// Routes
+app.use('/api', require('./routes/api'));
+
+// Start server
+app.listen(3000);
+console.log('API is running on port 3000');
+
+// Path: routes/api.js
+// REST API
+
+// Dependencies
+var express = require('express');
+var router = express.Router();
+
+// Models
+var Book = require('../models/Book');
+
+// Routes
+Book.methods(['get', 'put', 'post', 'delete']);
+Book.register(router, '/books');
+
+// Return router
+module.exports = router;
+
+// Path: models/Book.js
+// Book model
+
+// Dependencies
+var restful = require('node-restful');
+var mongoose = restful.mongoose;
+
+// Schema
+var bookSchema = new mongoose.Schema({
+    title: String,
